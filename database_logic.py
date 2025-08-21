bc1335235921463875c194906942ad6d7421b570
unknown <sitamurielkeita@gmail.com>
2025-08-21 12:19:38 +0000

    modifications 2

--- database_logic.py
+++ database_logic.py
@@ -46,9 +46,10 @@ def update_account_db(account_id, name, email, balance):
     """Met à jour un compte existant."""
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute(
-                "UPDATE accounts SET email = %s, balance = %s WHERE id = %s", # Il enlève 'name'
-                (email, balance or 0, account_id)
+            cur.execute( 
+                "UPDATE accounts SET balance = %s, name = %s, email = %s WHERE id = %s", # Il change l'ordre
+                (balance or 0, name, email, account_id)
+          
 
             )
 
